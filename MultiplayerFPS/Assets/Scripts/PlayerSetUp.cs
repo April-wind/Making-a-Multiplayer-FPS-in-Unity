fe93bdffea14cce65acad9f82b59ee16a1523ceb
jiang jie <2822569160@qq.com>
2019-11-05 22:17:21 +0800

    顶点着色器相关
    cginc文件自定义
    顶点着色器输入输出语义
    用结构体包装输入
    uniform关键字

---
+++ MultiplayerFPS/Assets/Scripts/PlayerSetUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetUp : NetworkBehaviour
+{	
+	[SerializeField]
+	private Behaviour[] componentsToDisable;
+
+	private Camera sceneCamera;
+	private void Start()
+	{
+		//如果不是当前窗口玩家
+		if (!isLocalPlayer)
+		{
+			for (int i = 0; i < componentsToDisable.Length; i++)
+			{
+				componentsToDisable[i].enabled = false;
+			}
+		}
+		else
+		{
+			sceneCamera = Camera.main;
+
+			//说明场景中不止一个camera 因为玩家自带一个
+			if (sceneCamera != null)
+			{
+				sceneCamera.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (sceneCamera != null)
+		{
+			sceneCamera.gameObject.SetActive(true);
+		}
+	}
+}
